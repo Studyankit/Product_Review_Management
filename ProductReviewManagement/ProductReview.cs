46d45d30df8250089553fb04d7dadbc2a58eaba0
StudyAnkit <ankitghosh2018@gmail.com>
2022-03-30 15:52:22 +0530

    [Ankit]- Added review specification

---
+++ ProductReviewManagement/ProductReview.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagement
+{
+    internal class ProductReview
+    {
+        private int ProductID { get; set; }
+        private int UserID { get; set; }
+        private int Rating { get; set; }
+        private int Review { get; set; }
+        private int isLike { get; set; }
+
+        public void ReviewSpecification(int ProductID, int UserID, int Rating, int Review, isLike)
+        {
+            this.ProductID = ProductID;
+            this.UserID = UserID;
+            this.Rating = Rating;
+            this.Review = Review;
+            this.isLike = isLike;
+            
+        }
+    }
+}
